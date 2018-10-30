3fedc6e776aeeee3b05735f169b53ab08d0c9268
Biller17 <abiller95@gmail.com>
2018-10-29 18:05:01 -0600

    .

--- Tarea4/histogram_eq_gpu.cu
+++ Tarea4/histogram_eq_gpu.cu
@@ -34,7 +34,7 @@ __global__ void create_image_histogram(char *input, int* histogram, int nx, int 
 }
 
 
-__global__ void normalize_histogram( char *input,int *histogram, int *normalized_histogram, int nx, int ny){
+__global__ void normalize_histogram(char *input,int *histogram, int *normalized_histogram, int nx, int ny){
 	unsigned int ix = threadIdx.x + blockIdx.x * blockDim.x;
 	unsigned int iy = threadIdx.y + blockIdx.y * blockDim.y;
 	unsigned int idx = iy * nx + ix;
@@ -43,7 +43,7 @@ __global__ void normalize_histogram( char *input,int *histogram, int *normalized
 		accumulated += histogram[x];
 	}
 
-	if(idx < 256){
+	if(idx < 255){
 		normalized_histogram[idx] = accumulated * 255 / (nx*ny);
 	}
 }
